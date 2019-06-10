7ca611f1a9ec3a1f792b076d3db8abdec6edc8f6
Linagrajales <lgrajalesv@unal.edu.co>
2019-06-10 17:42:47 -0500

    Final

--- script.sh
+++ script.sh
@@ -1,1 +1,23 @@
-# Escriba su código aquí
+for file in *.csv;
+    do sed '/^$/d' $file > "$(basename "$file" .csv)_1.csv";
+done
+
+for file in *_1.csv;
+    do  awk '{print FILENAME "," NR, ",",$0 }' $file > "$(basename "$file" .csv)_2.csv";
+done
+
+for file in *_2.csv;
+    do sed 's/[[:space:]]//g' $file > "$(basename "$file" .csv)_3.csv";
+done
+
+for file in *_3.csv;
+    do sed 's/\([A-Z]\)/\1,/g' $file > "$(basename "$file" .csv)_4.csv";
+done
+
+awk -F',' '{ for(i=4;i<=NF;i++) print $1,$2,$3,$i}' OFS=',' *_4.csv > output.csv
+
+sed -e 's/_1//g' output.csv > output1.csv
+
+rm *_1.csv  *_2.csv *_3.csv *_4.csv output.csv
+
+cat output1.csv
